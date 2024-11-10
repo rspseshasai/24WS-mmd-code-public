80249465572f57118b2fba2a3d45392cb6a21190
Pavan <rspseshasai@gmail.com>
2024-11-10 20:52:39 +0100

    problem set 3: add code for ex 4b

---
+++ albert_matrix_factorization/4a.py
@@ -0,0 +1,56 @@
+import numpy as np
+
+
+def matrix_factorization1(R, P, Q, K, steps=5000, alpha=0.0002, beta=0.02):
+    Q = Q.T
+    for step in range(steps):
+        for i in range(len(R)):
+            for j in range(len(R[i])):
+                if R[i][j] > 0:
+                    eij = R[i][j] - np.dot(P[i, :], Q[:, j])
+                    for k in range(K):
+                        P[i][k] = P[i][k] + alpha * (2 * eij * Q[k][j] - beta * P[i][k])
+                        Q[k][j] = Q[k][j] + alpha * (2 * eij * P[i][k] - beta * Q[k][j])
+        eR = np.dot(P, Q)
+        e = 0
+        for i in range(len(R)):
+            for j in range(len(R[i])):
+                if R[i][j] > 0:
+                    e = e + pow(R[i][j] - np.dot(P[i, :], Q[:, j]), 2)
+                    for k in range(K):
+                        e = e + (beta / 2) * (pow(P[i][k], 2) + pow(Q[k][j], 2))
+        if e < 0.001:
+            break
+    return P, Q.T
+
+
+# Define your 6x12 utility matrix
+R = [
+    [1, 0, 3, 0, 0, 5, 0, 0, 5, 0, 4, 0],
+    [0, 0, 5, 4, 0, 0, 4, 0, 0, 2, 1, 3],
+    [2, 4, 0, 1, 2, 0, 3, 0, 4, 3, 5, 0],
+    [0, 2, 4, 0, 5, 0, 0, 4, 0, 0, 2, 0],
+    [0, 0, 4, 3, 4, 2, 0, 0, 0, 0, 2, 5],
+    [1, 0, 3, 0, 3, 0, 0, 2, 0, 0, 4, 0],
+]
+
+R = np.array(R)
+
+N = len(R)
+M = len(R[0])
+K = 3
+
+P = np.random.rand(N, K)
+Q = np.random.rand(M, K)
+nP, nQ = matrix_factorization1(R, P, Q, K)
+
+print("Matrix P:")
+print(np.round(nQ, 1))
+
+print("\nMatrix Q:")
+print(np.round(nP, 1))
+
+nR = np.dot(nP, nQ.T)
+
+print("\nPredicted Matrix R (dot product of Q and P.T):")
+print(np.round(nR, 1))
